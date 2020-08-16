7aad85d927513c4e8d38986b6a966265fa02c307
Rob Hagemans <robhagemans@users.noreply.github.com>
2020-08-16 16:35:32 +0100

    remove bethe-xxz-extra-real

--- bethe-xxz-extra-real/square.cc
+++
@@ -1,8 +0,0 @@
-#include "square.h"
-
-const char* exc_IncompatibleSizes = "incompatible matrix sizes";
-const char* exc_BaldChicken = "cannot chop a row and column from a size one matrix";
-
-#include "square.def"
-
-
